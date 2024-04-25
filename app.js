c2f452e887c597d59df6be69acc793c507c4303b
AMAN7040 <amansingh79620@gmail.com>
2024-04-25 14:37:33 +0530

    learned components

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//React.createElement => Object => HTML element
 
-const parent = React.createElement('div', {id: 'parent'}, [
-    React.createElement('div', {id:'child'}, [
-        React.createElement('h1', {id:'heading'}, "THIS IS THE HEADING 🤬"),
-        React.createElement('p', {id:'para'}, 'THIS IS A PARAGRAPH abhi kya hi bol'),
-      ]),
-      React.createElement('div', {id:'child2'}, [
-        React.createElement('h2', {id:'heading2'}, "THIS IS THE HEADING-2"),
-        React.createElement('p', {id:'para2'}, 'THIS IS A PARAGRAPH-2'),
-      ])
-]);
+// const heading = React.createElement('h1', {id:'heading'} , "HELOOO AMAN this is react course 🫥");
+
+//JSX - is not HTML in JS but we can call it as HTML-like or XML-like syntax
+
+// const jsxHeading = <h1 id="jsxHeading">HEADING USING JSX</h1>
+
+const Title = () => (
+    <h2>Title of this page</h2>
+)
+
+//React Component          && also component composition - combining or nesting components 
+const HeadingComponent = () =>(
+     <div>
+        <h1>THIS IS FUNCTION COMPONENT</h1>
+     <Title/>
+    </div>
+)
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent)
+root.render(<HeadingComponent/>); 
