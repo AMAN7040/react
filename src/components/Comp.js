4a9e9f7f4e841af4fe1c975b136ef07116896702
AMAN7040 <amansingh79620@gmail.com>
2024-05-09 23:56:28 +0530

    class component

--- src/components/Comp.js
+++ src/components/Comp.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export const Comp = (props) => {
-    const {name, place, info} = props;
-  return (
-    <div className='comp'>
-     <h1>{name}</h1>
-     <h3>{place}</h3>
-     <h4>{info}</h4>
-    </div>
-  )
+// export const Comp = (props) => {
+//     const {name, place, info} = props;
+//   return (
+//     <div className='comp'>
+//      <h1>{name}</h1>
+//      <h3>{place}</h3>
+//      <h4>{info}</h4>
+//     </div>
+//   )
+// }
+
+class Comp extends Component{
+    constructor(props){
+        super(props); 
+    }
+  render(){
+    const {name,place, info} = this.props;
+    return (
+            <div className='comp'>
+             <h1>{name}</h1>
+             <h3>{place}</h3>
+             <h4>{info}</h4>
+            </div>
+          )
+  }
 }
 
 export default Comp;
