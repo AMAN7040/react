9f34c944c0fa8508eab64b7af2a8749eced1ee7e
AMAN7040 <amansingh79620@gmail.com>
2024-05-26 17:49:45 +0530

    lifecycle methods

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,38 @@
 import Comp from "./Comp";
+import { Component } from "react";
 
-const About = () =>{
-    return(
+// const About = () =>{
+//     return(
+//       <div className="aboutus">
+//         <h1>About</h1>
+//         <h3>This is a about page</h3>
+//         <Comp name={"Singh Aman"} place={'Delhi'} info={'Yolooo'}/>
+//       </div>
+//     )
+// }
+
+class About extends Component{
+  constructor(props){
+    super(props);
+
+   
+  }
+  
+  componentDidMount(){
+    
+  }
+
+  render(){
+   
+    return( 
       <div className="aboutus">
         <h1>About</h1>
         <h3>This is a about page</h3>
-        <Comp name={"Singh Aman"} place={'Delhi'} info={'Yolooo'}/>
+        <Comp/>
       </div>
     )
+  }
 }
+
 
 export default About;
--- src/components/Comp.js
+++ src/components/Comp.js
@@ -1,30 +1,94 @@
-import React, {Component} from 'react'
+import React, {Component, useEffect, useState} from 'react'
 
-// export const Comp = (props) => {
-//     const {name, place, info} = props;
-//   return (
-//     <div className='comp'>
-//      <h1>{name}</h1>
-//      <h3>{place}</h3>
-//      <h4>{info}</h4>
-//     </div>
-//   )
-// }
 
-class Comp extends Component{
-    constructor(props){
-        super(props); 
-    }
-  render(){
-    const {name,place, info} = this.props;
+const Comp = () => {
+  const [info, setInfo] = useState({});
+  
+  useEffect(()=>{
+    //API CALL
+    const fetchData = async () => {
+      try{
+        const data = await fetch('https://api.github.com/users/AMAN7040');
+        const json = await data.json();
+       setInfo(json)
+
+      }catch(error){
+        console.error('error fetching data', error);
+      }
+    };
+
+    const timer = setInterval(()=>{
+      console.log('Welcome Aman');
+     },1000)
+     console.log("useEffect called");
+    // fetchData();
+   
+    return ()=>{
+      clearInterval(timer);
+      console.log('useEffect returned');
+    };
+    
+  })
+
+  console.log("render")
+
+  const {login, avatar_url, followers} = info
+
     return (
             <div className='comp'>
-             <h1>{name}</h1>
-             <h3>{place}</h3>
-             <h4>{info}</h4>
+              <img src={avatar_url}/>
+             <h2>{login}</h2>
+             <h3>{followers}</h3>
             </div>
           )
-  }
 }
+
+// class Comp extends Component{
+//     constructor(props){
+//       super(props); 
+      
+//       this.state= {
+//         userInfo: {
+//           name: 'default',
+//           location: 'default'
+//         } 
+//       }
+//       console.log('component constructor');
+//     }
+   
+//     async componentDidMount(){
+//       const data = await fetch('https://api.github.com/users/AMAN7040');
+//       const json = await data.json();
+//       console.log('component mounted');
+
+//       this.timer = setInterval(()=>{
+//         console.log('Hiee Aman');
+//       },2000)
+
+//       this.setState({
+//         userInfo: json,
+//       })
+//     }   
+
+//     componentDidUpdate(){
+//       console.log('component updated');
+//     }
+//     componentWillUnmount(){
+//       clearInterval(this.timer);
+//       console.log('component unmounted');
+//     }
+
+//   render(){
+//     const {login, followers, avatar_url} = this.state.userInfo;
+//     return (
+//             <div className='comp'>
+//               <img src={avatar_url}/>
+//              <h2>{login}</h2>
+//              <h3>{followers}</h3>
+//             </div>
+//           )
+//           console.log('component rendered');
+//   }
+// }
 
 export default Comp;
